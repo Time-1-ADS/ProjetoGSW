0093e4330b935a266984b525fd4a66c05083e967
LucasACES <lucasc.a01@outlook.com>
2021-03-17 20:54:56 -0300

    teste

--- SPRINT_1/code/dashboard/dashboard/settings.py
+++ SPRINT_1/code/dashboard/dashboard/settings.py
@@ -56,7 +56,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            BASE_DIR / 'templates'
+            BASE_DIR / 'templates',
         ],
         'APP_DIRS': True,
         'OPTIONS': {
