cad4bd9e89b9cd178713367264339d0e4f02aa0e
Lucas Cesar <lucasc.a01@outlook.com>
2021-03-30 11:18:15 -0300

    add sprint2 | change to flask

---
+++ SPRINT_2/codes/main.py
@@ -0,0 +1,15 @@
+from flask import Flask, redirect, url_for, render_template
+import os
+
+
+app = Flask(__name__, static_folder='static')
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
