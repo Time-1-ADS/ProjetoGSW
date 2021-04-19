942a221fe07e90efc2830ac36a12d2f6f0a96348
mvarocha <marcus.varocha@gmail.com>
2021-04-19 12:32:21 -0300

    alterando cor dos gráficos

--- SPRINT_2/codes/web/static/js/main.js
+++ SPRINT_2/codes/web/static/js/main.js
@@ -6,13 +6,13 @@ var chart = new Chart(ctx, {
         datasets: [{
             label: 'Quantidade por Status',
             backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(255, 159, 64, 0.2)',
-                'rgba(255, 205, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(201, 203, 207, 0.2)'
+                'rgba(255, 99, 132)',
+                'rgba(255, 159, 64)',
+                'rgba(255, 205, 86)',
+                'rgba(75, 192, 192)',
+                'rgba(54, 162, 235)',
+                'rgba(153, 102, 255)',
+                'rgba(201, 203, 207)'
             ],
             borderColor: [
                 'rgb(255, 99, 132)',
@@ -48,8 +48,8 @@ var chart2 = new Chart(ctx2, {
         datasets: [{
             label: 'Tasks Abertas e Fechadas',
             backgroundColor: [
-                'rgba(0, 255, 0, 0.2)',
-                'rgba(255, 99, 132, 0.2)',
+                'rgba(0, 255, 0)',
+                'rgba(255, 99, 132)',
             ],
             borderColor: [
                 'rgb(0, 255, 0)',
