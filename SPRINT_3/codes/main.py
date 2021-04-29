bef5b0aa540be2b785db5baf3ce004aa1ac3ec8e
Marcus Vinícius <marcus.varocha@gmail.com>
2021-04-29 16:24:20 -0300

    cria pasta sprint 3

---
+++ SPRINT_3/codes/main.py
@@ -0,0 +1,32 @@
+from flask import Flask, redirect, url_for, render_template
+import os
+
+app = Flask(__name__,
+            static_url_path='',
+            static_folder='web/static',
+            template_folder='web/templates')
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/project')
+def projeto():
+    return render_template('projetoY.html')
+
+
+@app.route('/collaborators')
+def collaborators():
+    return render_template('colaboradores.html')
+
+
+@app.route('/task')
+def tasks():
+    return render_template('Tasks.html')
+
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
