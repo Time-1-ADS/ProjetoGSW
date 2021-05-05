3b2e748e32e882d66d675b81c02f2795dab6e07d
Marcus Vinícius <marcus.varocha@gmail.com>
2021-05-05 19:10:10 -0300

    gera arquivos de novas páginas

---
+++ SPRINT_3/codes/web/static/js/battask.js
@@ -0,0 +1,38 @@
+var albtask = document.getElementById('battask').getContext('2d');
+
+fetch('https://apigsw.herokuapp.com/data/task/batista/')
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (dados) {
+        console.log(dados)
+        var chart = new Chart(albtask, {
+            type: 'horizontalBar',
+            data: {
+
+                labels: ['Aberta', 'Fechada'],
+                datasets: [{
+                    label: 'Tasks',
+                    backgroundColor: [
+                        'rgba(28, 200, 138)',
+                        'rgba(231, 74, 59)'
+                    ],
+                    borderColor: [
+                        'rgb(28, 200, 138)',
+                        'rgb(231, 74, 59)'
+                    ],
+                    borderWidth: 3,
+                    data: [(dados[1][1]), (dados[2][1])],
+                }]
+            },
+            options: {
+                scales: {
+                    xAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }]
+                }
+            }
+        });
+    });
---
+++ SPRINT_3/codes/web/static/js/tasksgeral.js
@@ -0,0 +1,38 @@
+var ctx1 = document.getElementById('myChartGeral').getContext('2d');
+
+fetch('https://apigsw.herokuapp.com/data/task/')
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (dados) {
+        console.log(dados)
+        var chart = new Chart(ctx1, {
+            type: 'horizontalBar',
+            data: {
+
+                labels: ['Aberta', 'Fechada'],
+                datasets: [{
+                    label: 'Tasks',
+                    backgroundColor: [
+                        'rgba(28, 200, 138)',
+                        'rgba(231, 74, 59)'
+                    ],
+                    borderColor: [
+                        'rgb(28, 200, 138)',
+                        'rgb(231, 74, 59)'
+                    ],
+                    borderWidth: 3,
+                    data: [(dados[1][1]), (dados[2][1])],
+                }]
+            },
+            options: {
+                scales: {
+                    xAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }]
+                }
+            }
+        });
+    });
