b4617b225a088a61f27f6d2aaa1e98bfee1f94a7
mvarocha <marcus.varocha@gmail.com>
2021-05-28 09:56:02 -0300

    responsividade e menu lateral

---
+++ SPRINT_4/codes/static/js/graficos-projetos/task/batista.js
@@ -0,0 +1,28 @@
+var ctx = document.getElementById('Task').getContext('2d');
+
+fetch('https://apigsw.herokuapp.com/data/task/batista/')
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (dados) {
+        console.log(dados)
+        var chart = new Chart(ctx, {
+            type: 'doughnut',
+            data: {
+
+                labels: ['Fechada', 'Aberta'],
+                datasets: [{
+                    label: 'Tasks',
+                    backgroundColor: [
+                        'rgba(231, 74, 59)',
+                        'rgba(28, 200, 138)'
+                    ],
+                    borderColor: [
+                        'rgba(231, 74, 59)',
+                        'rgba(28, 200, 138)'
+                    ],
+                    data: [(dados[1][1]), (dados[2][1])],
+                }]
+            },
+        });
+    });
