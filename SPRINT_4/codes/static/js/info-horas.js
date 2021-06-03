b715fa30b6e30c1c68087d375f5470747ec3153c
raphael <raphaelprado2011@hotmail.com>
2021-06-03 15:13:55 -0300

    atualizando colaboradores e horas dedicadas

---
+++ SPRINT_4/codes/static/js/info-horas.js
@@ -0,0 +1,321 @@
+//PABLO
+$(document).ready(function () {
+    $(function () {
+        fetch('http://apigsw.herokuapp.com/data/horascolaborador/pablomartins/')
+            .then(function (task) {
+                return task.json();
+            })
+            .then(function (dados) {
+                $("colab475").append(dados[1]),
+                    $("colab476").append(dados[2]);
+                $("colab477").append(dados[4]);
+                $("colab478").append(dados[5]);
+            });
+    });
+});
+
+//ELISIO MARTINS
+$(document).ready(function () {
+    $(function () {
+        fetch('http://apigsw.herokuapp.com/data/horascolaborador/elisiomartins/')
+            .then(function (task) {
+                return task.json();
+            })
+            .then(function (dados) {
+                $("colab479").append(dados[1]),
+                    $("colab480").append(dados[2]);
+                $("colab481").append(dados[4]);
+                $("colab482").append(dados[5]);
+            });
+    });
+});
+
+//CARLOS
+$(document).ready(function () {
+    $(function () {
+        fetch('http://apigsw.herokuapp.com/data/horascolaborador/carlosbatista/')
+            .then(function (task) {
+                return task.json();
+            })
+            .then(function (dados) {
+                $("colab483").append(dados[1]),
+                    $("colab484").append(dados[2]);
+                $("colab485").append(dados[4]);
+                $("colab486").append(dados[5]);
+            });
+    });
+});
+
+//VICENTE
+$(document).ready(function () {
+    $(function () {
+        fetch('http://apigsw.herokuapp.com/data/horascolaborador/vicentemoraes/')
+            .then(function (task) {
+                return task.json();
+            })
+            .then(function (dados) {
+                $("colab487").append(dados[1]),
+                    $("colab488").append(dados[2]);
+                $("colab489").append(dados[4]);
+                $("colab490").append(dados[5]);
+            });
+    });
+});
+
+//ANA JULIA
+$(document).ready(function () {
+    $(function () {
+        fetch('http://apigsw.herokuapp.com/data/horascolaborador/anajulia/')
+            .then(function (task) {
+                return task.json();
+            })
+            .then(function (dados) {
+                $("colab491").append(dados[1]),
+                    $("colab492").append(dados[2]);
+                $("colab493").append(dados[4]);
+                $("colab494").append(dados[5]);
+            });
+    });
+});
+
+//DAVI
+$(document).ready(function () {
+    $(function () {
+        fetch('http://apigsw.herokuapp.com/data/horascolaborador/davibatista/')
+            .then(function (task) {
+                return task.json();
+            })
+            .then(function (dados) {
+                $("colab495").append(dados[1]),
+                    $("colab496").append(dados[2]);
+                $("colab497").append(dados[4]);
+                $("colab498").append(dados[5]);
+            });
+    });
+});
+
+//BERNARDO
+$(document).ready(function () {
+    $(function () {
+        fetch('http://apigsw.herokuapp.com/data/horascolaborador/bernardosouza/')
+            .then(function (task) {
+                return task.json();
+            })
+            .then(function (dados) {
+                $("colab499").append(dados[1]),
+                    $("colab500").append(dados[2]);
+                $("colab501").append(dados[4]);
+                $("colab502").append(dados[5]);
+            });
+    });
+});
+
+//GABRIEL
+$(document).ready(function () {
+    $(function () {
+        fetch('http://apigsw.herokuapp.com/data/horascolaborador/gabrielpereira/')
+            .then(function (task) {
+                return task.json();
+            })
+            .then(function (dados) {
+                $("colab503").append(dados[1]),
+                    $("colab504").append(dados[2]);
+                $("colab505").append(dados[4]);
+                $("colab506").append(dados[5]);
+            });
+    });
+});
+
+//CELIA
+$(document).ready(function () {
+    $(function () {
+        fetch('http://apigsw.herokuapp.com/data/horascolaborador/celiaoliveira/')
+            .then(function (task) {
+                return task.json();
+            })
+            .then(function (dados) {
+                $("colab507").append(dados[1]),
+                    $("colab508").append(dados[2]);
+                $("colab509").append(dados[4]);
+                $("colab510").append(dados[5]);
+            });
+    });
+});
+
+//ANTONIO
+$(document).ready(function () {
+    $(function () {
+        fetch('http://apigsw.herokuapp.com/data/horascolaborador/antonionogueira/')
+            .then(function (task) {
+                return task.json();
+            })
+            .then(function (dados) {
+                $("colab511").append(dados[1]),
+                    $("colab512").append(dados[2]);
+                $("colab513").append(dados[4]);
+                $("colab514").append(dados[5]);
+            });
+    });
+});
+
+//ENZO
+$(document).ready(function () {
+    $(function () {
+        fetch('http://apigsw.herokuapp.com/data/horascolaborador/enzogabriel/')
+            .then(function (task) {
+                return task.json();
+            })
+            .then(function (dados) {
+                $("colab515").append(dados[1]),
+                    $("colab516").append(dados[2]);
+                $("colab517").append(dados[4]);
+                $("colab518").append(dados[5]);
+            });
+    });
+});
+
+//FABIO
+$(document).ready(function () {
+    $(function () {
+        fetch('http://apigsw.herokuapp.com/data/horascolaborador/fabiosouza/')
+            .then(function (task) {
+                return task.json();
+            })
+            .then(function (dados) {
+                $("colab519").append(dados[1]),
+                    $("colab520").append(dados[2]);
+                $("colab521").append(dados[4]);
+                $("colab522").append(dados[5]);
+            });
+    });
+});
+
+//LUCCA
+$(document).ready(function () {
+    $(function () {
+        fetch('http://apigsw.herokuapp.com/data/horascolaborador/luccamelo/')
+            .then(function (task) {
+                return task.json();
+            })
+            .then(function (dados) {
+                $("colab523").append(dados[1]),
+                    $("colab524").append(dados[2]);
+                $("colab525").append(dados[4]);
+                $("colab526").append(dados[5]);
+            });
+    });
+});
+
+//RAFAEL
+$(document).ready(function () {
+    $(function () {
+        fetch('http://apigsw.herokuapp.com/data/horascolaborador/rafaelxavier/')
+            .then(function (task) {
+                return task.json();
+            })
+            .then(function (dados) {
+                $("colab527").append(dados[1]),
+                    $("colab528").append(dados[2]);
+                $("colab529").append(dados[4]);
+                $("colab530").append(dados[5]);
+            });
+    });
+});
+
+//MATHEUS
+$(document).ready(function () {
+    $(function () {
+        fetch('http://apigsw.herokuapp.com/data/horascolaborador/matheusbarros/')
+            .then(function (task) {
+                return task.json();
+            })
+            .then(function (dados) {
+                $("colab531").append(dados[1]),
+                    $("colab532").append(dados[2]);
+                $("colab533").append(dados[4]);
+                $("colab534").append(dados[5]);
+            });
+    });
+});
+
+//GUSTAVO
+$(document).ready(function () {
+    $(function () {
+        fetch('http://apigsw.herokuapp.com/data/horascolaborador/gustavosantos/')
+            .then(function (task) {
+                return task.json();
+            })
+            .then(function (dados) {
+                $("colab535").append(dados[1]),
+                    $("colab536").append(dados[2]);
+                $("colab537").append(dados[4]);
+                $("colab538").append(dados[5]);
+            });
+    });
+});
+
+//TALITA
+$(document).ready(function () {
+    $(function () {
+        fetch('http://apigsw.herokuapp.com/data/horascolaborador/talitabraga/')
+            .then(function (task) {
+                return task.json();
+            })
+            .then(function (dados) {
+                $("colab539").append(dados[1]),
+                    $("colab540").append(dados[2]);
+                $("colab541").append(dados[4]);
+                $("colab542").append(dados[5]);
+            });
+    });
+});
+
+//ELISIO ALBUQUERQUE
+$(document).ready(function () {
+    $(function () {
+        fetch('http://apigsw.herokuapp.com/data/horascolaborador/elisioalbuquerque/')
+            .then(function (task) {
+                return task.json();
+            })
+            .then(function (dados) {
+                $("colab543").append(dados[1]),
+                    $("colab544").append(dados[2]);
+                $("colab545").append(dados[4]);
+                $("colab546").append(dados[5]);
+            });
+    });
+});
+
+//CECILIA
+$(document).ready(function () {
+    $(function () {
+        fetch('http://apigsw.herokuapp.com/data/horascolaborador/ceciliamoreira/')
+            .then(function (task) {
+                return task.json();
+            })
+            .then(function (dados) {
+                $("colab547").append(dados[1]),
+                    $("colab548").append(dados[2]);
+                $("colab549").append(dados[4]);
+                $("colab550").append(dados[5]);
+            });
+    });
+});
+
+//KLEBER
+$(document).ready(function () {
+    $(function () {
+        fetch('http://apigsw.herokuapp.com/data/horascolaborador/kleberoliveira/')
+            .then(function (task) {
+                return task.json();
+            })
+            .then(function (dados) {
+                $("colab556").append(dados[1]),
+                    $("colab557").append(dados[2]);
+                $("colab558").append(dados[4]);
+                $("colab559").append(dados[5]);
+            });
+    });
+});
+
+
