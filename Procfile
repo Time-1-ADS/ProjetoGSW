45ea7ff4d71b02bfb1c54fc69662b0211b0f4a40
Lucas César <lucasc.a01@outlook.com>
2021-06-05 23:21:01 -0300

    try fix error

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn app:my_app
+web: gunicorn --bind 0.0.0.0:$PORT app:my_app
--- __init__.py
+++ __init__.py
@@ -35,7 +35,6 @@ def create_app():
     app.register_blueprint(main_blueprint)
 
     if __name__ == '__main__':
-        port = int(os.environ.get("PORT", 5000))
-        app.run(host='0.0.0.0', port=port, debug=False)
+        app.run()
 
     return app
