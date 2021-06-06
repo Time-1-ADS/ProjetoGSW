5ab50d4c332319a50e296eff1374e99e80ec5371
Lucas César <lucasc.a01@outlook.com>
2021-06-05 23:05:45 -0300

    try fix error

--- __init__.py
+++ __init__.py
@@ -34,8 +34,8 @@ def create_app():
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
-    if __name__ == '__main__':
-        port = int(os.environ.get("PORT", 5000))
-        app.run(host='0.0.0.0', port=port, debug=False)
+    # if __name__ == '__main__':
+    #     port = int(os.environ.get("PORT", 5000))
+    #     app.run(host='0.0.0.0', port=port, debug=False)
 
     return app
