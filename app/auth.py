8d116a5f72394c6a3f052d643761535d89432765
Lucas César <lucasc.a01@outlook.com>
2021-06-06 00:19:22 -0300

    try fix error

--- app/auth.py
+++ app/auth.py
@@ -13,7 +13,7 @@ auth = Blueprint('auth', __name__)
 
 @auth.route('/login')
 def login():
-    return render_template('login.html')
+    return render_template('Login.html')
 
 
 @auth.route('/signup')
