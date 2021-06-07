d5c4e3c04dccb3e98ab2ab670c0c4725d1013427
Lucas César <lucasc.a01@outlook.com>
2021-06-07 20:54:34 -0300

    try fix error

--- app/auth.py
+++ app/auth.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template, redirect, url_for, request, flash
 from flask.helpers import flash
 from flask_user import roles_required
 from flask_login import login_user, logout_user, login_required
-from werkzeug.security import generate_password_hash, check_password_hash, Unauthorized
+from werkzeug.security import generate_password_hash, check_password_hash
 from .models import User, Role, UserRoles
 from .init import db
 import email_validator
