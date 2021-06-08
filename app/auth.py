9776343d9acffd0ad49522a84ac14a4539dba43d
Lucas César <lucasc.a01@outlook.com>
2021-06-08 00:28:23 -0300

    try fix error

--- app/auth.py
+++ app/auth.py
@@ -53,7 +53,7 @@ def signup_post():
     name = request.form.get('name')
     password = request.form.get('password')
     project = request.form.get('project')
-    role = request.form.get('cargo')
+    roles = request.form.get('cargo')
     db.create_all()
 
     user = User.query.filter_by(email=email).first()
@@ -63,7 +63,7 @@ def signup_post():
         return redirect(url_for('auth.signup'))
 
     
-    new_user = User(email=email, name=name, password=generate_password_hash(password, method='sha256'), project=project, role=role)
+    new_user = User(email=email, name=name, password=generate_password_hash(password, method='sha256'), project=project, roles=roles)
     db.session.add(new_user)
     db.session.commit()
     
